c4bbf1d2068f8a7dc83a616e73fad5f85e14a417
Jonathan W. Taylor <elderling@gmail.com>
2022-04-14 11:53:26 -0500

    Initial commit of unit test

---
+++ less_test.c
@@ -0,0 +1,32 @@
+#include "Unity/src/unity.h"
+#include "less.h"
+
+void setUp(void)
+{
+    return;
+}
+
+void tearDown(void)
+{
+    return;
+}
+
+void test_ok(void);
+
+void test_ok(void)
+{
+    TEST_ASSERT_EQUAL_STRING("ok", "ok");
+
+    return;
+}
+
+
+int main(void)
+{
+    UNITY_BEGIN();
+
+    RUN_TEST(test_ok);
+
+    return UNITY_END();
+}
+
